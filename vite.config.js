247601b16359acc2ebbed07e7d826b2047c964cd
Isaac Hermel <isaachermel@gmail.com>
2023-08-10 19:43:12 -0300

    added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-undef */
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 //* Plugins
@@ -12,7 +13,45 @@ export default defineConfig({
     react({
       include: "**/*.jsx",
     }),
-    postcss()
+    postcss(),
+    VitePWA({
+      manifest: {
+        theme_color: "#5616c5",
+        background_color: "#191e24",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "SenFinança",
+        description: "Your personal finance management app.",
+        name: "SenFinança",
+        icons: [
+          {
+            src: "images/favicon/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "images/favicon/manifest-icon-192.maskable.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "images/favicon/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "images/favicon/manifest-icon-512.maskable.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
   ],
   server: {
     hmr: true,
